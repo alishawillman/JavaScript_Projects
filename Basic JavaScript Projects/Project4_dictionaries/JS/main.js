2f495decef5691fcdee68e8284ca136a3aaebfe2
alishawillman <72734094+alishawillman@users.noreply.github.com>
2021-08-17 15:54:57 -0600

    JavaScript Project 4
    Fixed missing closing tag,  "," and "()"

--- Basic JavaScript Projects/Project4_dictionaries/JS/main.js
+++ Basic JavaScript Projects/Project4_dictionaries/JS/main.js
@@ -2,8 +2,8 @@ function my_Dictionary () { //defining a function
     var Animal = { //defining variables
         Species: "Dog",
         Color: "Black & Tan",
-        Breed: "Yorkshire Terrier"
-        Age: 10
+        Breed: "Yorkshire Terrier",
+        Age: 10,
         Sound: "Bark",
     };
     delete Animal.Breed; //utilizing delete operator
