2ce204e5a3512b1dd630b7a146686b7eed763037
alishawillman <72734094+alishawillman@users.noreply.github.com>
2021-08-16 21:22:10 -0600

    Basic JavaScript Submission Assignment
    Basic JavaScript

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,28 @@
+window.alert("Hello, world!"); //Assigning a window alert
+
+document.write('She said, \"Hello, Alisha!\"' //Creating a string that is concatenated
++ 'and then they sat at a table'
++ 'and drank coffee');
+
+var A = "This is a string"; //Assigning a variable and giving it a value
+
+document.write(A); //Printing variable A
+
+var B= "This is" + "my concatenated string" //Creating a string that is concatenated
+document.write(B) //printing concatenated string
+
+10+50; //This is an expression
+document.write(10+50); //Statement containing an expression
+
+
+var Dogs= "The Willmans", Zeus= "Yorkie", Luna= "Minature Poodle", Gus= "Border Collie", Roscoe= "Yorkie", Roxy= "Yorkie"; //Assigning multiple variables in one statement
+var Dogs= Dogs.fontcolor("purple"); //Using the fontcolor method on string
+var Zeus= Zeus.fontcolor("Green"); //Using the fontcolor method on string
+var Luna= Luna.fontcolor("yellow"); //Using the fontcolor method on string
+var Gus= Gus.fontcolor("red"); //Using the fontcolor method on string
+var Roscoe= Roscoe.fontcolor("orange"); //Using the fontcolor method on string
+var Roxy= Roxy.fontcolor("pink"); //Using the fontcolor method on string
+document.write(Zeus); //Printing variable in color
+
+var Name= "Alisha Willman"; //Assigning a variable and giving it a string value
+document.write(Name)  //Putting the value into html element
